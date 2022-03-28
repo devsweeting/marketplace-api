faf07134fe45768132d136507aa69b549f12e54e
Yana Kłodkowska <yana.klodkowska@rst.com.pl>
2022-03-28 15:06:38 +0200

    [#1495] rollbar

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-rollbar';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,22 +14,42 @@ import { AssetsModule } from 'modules/assets/assets.module';
 import { join } from 'path';
 import { StorageModule } from 'modules/storage/storage.module';
 
+const modules = [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: [
+      `.env.${process.env.NODE_ENV}.local`,
+      `.env.${process.env.NODE_ENV}`,
+      '.env.local',
+      '.env',
+    ],
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    load: Object.values(require('./config')),
+  }),
+];
+
+if (process.env.NODE_ENV === 'STAGING' || process.env.NODE_ENV === 'PRODUCTION') {
+  modules.push(
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          accessToken: configService.get('rollbar.default.rollbarToken'),
+          environment: configService.get('rollbar.default.rollbarEnvironment'),
+          captureUncaught: true,
+          captureUnhandledRejections: true,
+          captureEmail: true,
+          captureUsername: true,
+        };
+      },
+    }),
+  );
+}
 @Module({
   imports: [
     AuthModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [
-        `.env.${process.env.NODE_ENV}.local`,
-        `.env.${process.env.NODE_ENV}`,
-        '.env.local',
-        '.env',
-      ],
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      load: Object.values(require('./config')),
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
@@ -39,6 +60,7 @@ import { StorageModule } from 'modules/storage/storage.module';
     PartnersModule,
     AssetsModule,
     UsersModule,
+    ...modules,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/exception.filter.ts
+++ src/exception.filter.ts
@@ -1,13 +1,36 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { RollbarLogger } from 'nestjs-rollbar';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const request: any = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    if (process.env.NODE_ENV === 'STAGING' || process.env.NODE_ENV === 'PRODUCTION') {
+      const rollbarLogger = new RollbarLogger({
+        accessToken: process.env.ROLLBAR_TOKEN,
+        environment: process.env.ROLLBAR_ENVIRONMENT,
+        captureUncaught: true,
+        captureUnhandledRejections: true,
+        captureEmail: true,
+        captureUsername: true,
+      });
+
+      if (request.user) {
+        rollbarLogger.configure({
+          payload: {
+            person: {
+              id: request.user.id,
+              username: request.user.name,
+            },
+          },
+        });
+      }
+      rollbarLogger.error(exception, `${request.method} ${request.url}`);
+    }
     Logger.error(`${request.method} ${request.url}`, exception.getResponse(), 'ExceptionFilter');
     response.status(status).json(exception.getResponse());
   }
