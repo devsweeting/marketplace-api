55966f1c19db9af0c6e7a3024ec9aaa6dad86098
Yana Kłodkowska <yana.klodkowska@rst.com.pl>
2022-05-10 11:18:18 +0200

    redirect to url #2231

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Redirect, VERSION_NEUTRAL } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller({
   version: VERSION_NEUTRAL,
 })
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect()
+  redirectToRootUrl() {
+    const url = this.configService.get('common.default.redirectRootUrl');
+    return url?.length ? { statusCode: HttpStatus.FOUND, url } : this.appService.getHello();
   }
 }
