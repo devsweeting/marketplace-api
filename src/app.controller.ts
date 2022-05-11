af4a45493180692e573032b088ecffc99d75e7d8
Yana Kłodkowska <yana.klodkowska@rst.com.pl>
2022-05-11 16:52:40 +0200

    delete random quote code #2231

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,16 @@
 import { Controller, Get, HttpStatus, Redirect, VERSION_NEUTRAL } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AppService } from './app.service';
 
 @Controller({
   version: VERSION_NEUTRAL,
 })
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly configService: ConfigService) {}
 
   @Get()
   @Redirect()
   redirectToRootUrl() {
     const url = this.configService.get('common.default.redirectRootUrl');
-    return url?.length ? { statusCode: HttpStatus.FOUND, url } : this.appService.getHello();
+    return { statusCode: HttpStatus.FOUND, url };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,31 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
-export const quotes = [
-  // Hackers
-  'Mess with the best, die like the rest.',
-  'Hack the Planet!',
-  'Never fear, I is here.',
-  "Never send a boy to do a woman's job.",
-  // The Matrix
-  'The Fight for the Future Begins.',
-  'Reality is a thing of the past.',
-  'Unfortunately, no one can be told what the Matrix is. You have to see it for yourself.',
-  'There is no spoon.',
-  'Follow the white rabbit.',
-  'In a world of 1s and 0s... are you a zero, or The One?',
-  // Eddy Van Halen
-  'Rock stars come and go. Musicians play until they die.',
-  'To hell with the rules. If it sounds right, then it is.',
-  "Bass is for people who can't play guitar.",
-  // David Lee Roth
-  "It doesn't get better, it doesn't get worse, but it sure gets different!",
-  // Van Halen
-  "You gotta roll with the punches to get to what's real",
-];
-
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return quotes[Math.floor(Math.random() * quotes.length)];
-  }
-}
+export class AppService {}
