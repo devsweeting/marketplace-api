67280ee7bed9db1eb1aa8543638e4441a6f78ff9
Jared Harper <jared.kuolt@gmail.com>
2022-05-11 10:55:08 -0700

    fix it in the right place

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -1,18 +1,15 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('aws', () => {
-  const defaultConfig = {
-    accessKey: process.env.AWS_ACCESS_KEY,
-    secretKey: process.env.AWS_SECRET_KEY,
-    s3Bucket: process.env.AWS_S3_BUCKET,
-    region: process.env.AWS_REGION,
-    cloudFrontDomain: process.env.CLOUDFRONT_DOMAIN,
-    s3ForcePathStyle: process.env.NODE_ENV === 'DEVELOP' || process.env.NODE_ENV === 'TEST',
-  };
-  if (process.env.AWS_ENDPOINT) {
-    defaultConfig['endpoint'] = process.env.AWS_ENDPOINT;
-  }
   return {
-    default: defaultConfig,
+    default: {
+      accessKey: process.env.AWS_ACCESS_KEY,
+      secretKey: process.env.AWS_SECRET_KEY,
+      s3Bucket: process.env.AWS_S3_BUCKET,
+      region: process.env.AWS_REGION,
+      cloudFrontDomain: process.env.CLOUDFRONT_DOMAIN,
+      endpoint: process.env.AWS_ENDPOINT,
+      s3ForcePathStyle: process.env.NODE_ENV === 'DEVELOP' || process.env.NODE_ENV === 'TEST',
+    },
   };
 });
