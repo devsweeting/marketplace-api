4668e5f0b5caad7039e147fc7cd59ed63dd2d682
Yana Kłodkowska <yana.klodkowska@rst.com.pl>
2022-06-21 15:46:27 +0200

    watchlist pagination #137

--- src/modules/watchlists/entities/watchlist.entity.ts
+++ src/modules/watchlists/entities/watchlist.entity.ts
@@ -1,9 +1,18 @@
-import { Column, Entity, JoinColumn, OneToOne, RelationId, OneToMany } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  RelationId,
+  OneToMany,
+  SelectQueryBuilder,
+} from 'typeorm';
 
 import { BaseEntityInterface } from 'modules/common/entities/base.entity.interface';
 import { BaseModel } from 'modules/common/entities/base.model';
 import { User } from 'modules/users/entities/user.entity';
 import { WatchlistAsset } from './watchlist-asset.entity';
+import { ListWatchlistDto } from '../dto';
 
 @Entity('watchlist')
 export class Watchlist extends BaseModel implements BaseEntityInterface {
@@ -21,5 +30,14 @@ export class Watchlist extends BaseModel implements BaseEntityInterface {
   constructor(partial: Partial<Watchlist>) {
     super();
     Object.assign(this, partial);
+  }
+
+  public static list(params: ListWatchlistDto, user: User): SelectQueryBuilder<WatchlistAsset> {
+    return WatchlistAsset.createQueryBuilder('watchlistAsset')
+      .leftJoinAndMapOne('watchlistAsset.watchlist', 'watchlistAsset.watchlist', 'watchlist')
+      .leftJoinAndMapOne('watchlistAsset.asset', 'watchlistAsset.asset', 'asset')
+      .where('watchlist.userId = :userId', { userId: user.id })
+      .andWhere('watchlistAsset.isDeleted = FALSE AND watchlistAsset.deletedAt IS NULL')
+      .addOrderBy(params.sort, params.order);
   }
 }
