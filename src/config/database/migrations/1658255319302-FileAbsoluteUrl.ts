2518ab6f5b6cfbda20162f2fe9f6a0ea676906fc
Jared Harper <jared.kuolt@gmail.com>
2022-07-20 14:00:21 -0700

    Fix broken migration

--- src/config/database/migrations/1658255319302-FileAbsoluteUrl.ts
+++ src/config/database/migrations/1658255319302-FileAbsoluteUrl.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class FileAbsoluteUrl1658255319302 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "files" ADD "absoluteUrl" character varying NOT NULL DEFAULT ''`,
+      `ALTER TABLE "files" ADD "absoluteUrl" character varying NULL DEFAULT ''`,
     );
   }
 
