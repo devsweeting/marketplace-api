9776148a4cf0258c1cf23351e8199f342f2eae87
Jared Harper <jared.kuolt@gmail.com>
2022-08-17 16:30:38 +0000

    Fix list test to include sell orders
    Fixes #215

--- test/utils/sell-order.utils.ts
+++ test/utils/sell-order.utils.ts
@@ -1,5 +1,6 @@
 import faker from '@faker-js/faker';
 import { SellOrder } from 'modules/sell-orders/entities';
+import { SellOrderTypeEnum } from 'modules/sell-orders/enums/sell-order-type.enum';
 
 export const createSellOrder = (data: Partial<SellOrder>): Promise<SellOrder> => {
   const sellOrder = new SellOrder({
@@ -7,6 +8,7 @@ export const createSellOrder = (data: Partial<SellOrder>): Promise<SellOrder> =>
     fractionPriceCents: 1000,
     startTime: Date.now(),
     expireTime: faker.date.future().getTime(),
+    type: SellOrderTypeEnum.standard,
     ...data,
   });
   return sellOrder.save();
