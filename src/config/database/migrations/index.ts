1dfdf426146c6fe0a257a178282e2ced4eb2b478
Levi Zitting <me@levizitting.com>
2022-09-04 18:50:12 -0500

    fix: update cli and related config to run migrations

---
+++ src/config/database/migrations/index.ts
@@ -0,0 +1,31 @@
+import { Init1652279127303 } from './1652279127303-Init';
+import { AssetIndexing1653052300775 } from './1653052300775-AssetIndexing';
+import { UserOtp1653559169560 } from './1653559169560-UserOtp';
+import { UpdateContractAndTokenTable1655296809164 } from './1655296809164-UpdateContractAndTokenTable';
+import { CreateSellOrderTable1657555594640 } from './1657555594640-CreateSellOrderTable';
+import { FileAbsoluteUrl1658255319302 } from './1658255319302-FileAbsoluteUrl';
+import { AddAssetAttributeJSONColumn1659551642976 } from './1659551642976-AddAssetAttributeJSONColumn';
+import { FixAttrsLowerCase1660061994750 } from './1660061994750-FixAttrsLowerCase';
+import { AddFractionQtyTotal1660149486573 } from './1660149486573-AddFractionQtyTotal';
+import { AddSellOrderColumns1660154769352 } from './1660154769352-AddSellOrderColumns';
+import { CreateSellOrderPurchasesTable1660164905288 } from './1660164905288-CreateSellOrderPurchasesTable';
+import { ModifySellOrderDateColumns1660760132910 } from './1660760132910-ModifySellOrderDateColumns';
+import { AddDropLimits1660941495793 } from './1660941495793-AddDropLimits';
+import { RenameUrlToSourceUrl1661270005764 } from './1661293286632-RenameUrlToSourceUrl';
+
+export const migrations = [
+  Init1652279127303,
+  AssetIndexing1653052300775,
+  UserOtp1653559169560,
+  UpdateContractAndTokenTable1655296809164,
+  CreateSellOrderTable1657555594640,
+  FileAbsoluteUrl1658255319302,
+  AddAssetAttributeJSONColumn1659551642976,
+  FixAttrsLowerCase1660061994750,
+  AddFractionQtyTotal1660149486573,
+  AddSellOrderColumns1660154769352,
+  CreateSellOrderPurchasesTable1660164905288,
+  ModifySellOrderDateColumns1660760132910,
+  AddDropLimits1660941495793,
+  RenameUrlToSourceUrl1661270005764,
+];
