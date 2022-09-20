982a9bcb9b24f1197f88bc593eddcda74e2f8be5
Jared Harper <jared.kuolt@gmail.com>
2022-09-20 16:18:10 +0000

    Allow mail-related exception to bubble up

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,4 +1,4 @@
-import { BadGatewayException, Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { SentMessageInfo } from 'nodemailer';
 import mailInfo from './interfaces/mailInfo.interface';
@@ -8,18 +8,11 @@ export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
   public async send({ emailTo, content, template }: mailInfo): Promise<SentMessageInfo> {
-    try {
-      await this.mailerService.sendMail({
-        to: emailTo,
-        subject: content.subject,
-        template: template,
-        context: { content: content },
-      });
-    } catch (e) {
-      // TODO: Logger.error does not show the error stack
-      console.log(e);
-      Logger.log(`MailService.send(${emailTo})`);
-      throw new BadGatewayException('Email send failed');
-    }
+    await this.mailerService.sendMail({
+      to: emailTo,
+      subject: content.subject,
+      template: template,
+      context: { content: content },
+    });
   }
 }
