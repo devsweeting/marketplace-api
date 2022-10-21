92e4f68a8ed80a3a0eda4453303a2baf93515002
nb-midwestern <nb@buildmidwestern.com>
2022-10-21 11:36:47 -0500

    remove comment

--- src/modules/portfolio/responses/portfolio.response.ts
+++ src/modules/portfolio/responses/portfolio.response.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { AssetResponse } from 'modules/assets/responses/asset.response';
 import { PaginatedResponse } from 'modules/common/dto/paginated.response';
 
-//TODO update paginatedResponse to somehow include a wrapper so that the type show up in swagger
 export class PortfolioResponse extends PaginatedResponse<AssetResponse> {
   @ApiProperty({ example: 1000 })
   totalValueInCents: number;
